f9f95ccf081e945c9e39e2318ad8f7e46cec7fd4
Matthew <dydals3440@gmail.com>
2024-01-04 22:37:11 +0900

    feat(postpreview.tsx): hover효과 추가
    hover효과 추가 및 다크모드 색상 개선

--- app/components/post/PostPreview.tsx
+++ app/components/post/PostPreview.tsx
@@ -20,15 +20,19 @@ const PostPreview: React.FC<PostPreviewProps> = ({ data }) => {
   }, [data.createdAt]);
 
   return (
-    <div
-      className="flex flex-col"
-      onClick={() => {
-        router.push(`/post/${data.id}`);
-      }}
-    >
-      <div className="font-regular text-neutral-500 text-sm truncate">{data.title}</div>
-      <div className="font-light text-xs text-neutral-300 truncate mt-1">{date}</div>
-      <div className="border-b-2 border-slate-50 mt-2"></div>
+    <div className="rounded-lg p-2 hover:bg-slate-300   hover:dark:bg-slate-500 transition cursor-pointer">
+      <div
+        className="flex flex-col "
+        onClick={() => {
+          router.push(`/post/${data.id}`);
+        }}
+      >
+        <div className="font-regular dark:text-neutral-400 text-sm truncate">{data.title}</div>
+        <div className="font-light text-xs dark:text-blue-400 text-neutral-600 truncate mt-1">
+          {date}
+        </div>
+        <div className="border-b-2 border-slate-50 dark:border-blue-300 mt-2"></div>
+      </div>
     </div>
   );
 };
