4a061ea6c3aa173dd63db544a6eb9bf678ce6523
Matthew <dydals3440@gmail.com>
2024-01-04 18:27:34 +0900

    feat(recruitments/[postid]): postGrid클릭시 라우팅 추가
    postGrid 클릭시, 해당 포스트의 아이디로 라우팅됨.

---
+++ app/recruitments/[postId]/page.tsx
@@ -0,0 +1,7 @@
+import Container from '@/app/components/Container';
+
+const RecruitmentsDetailPage = () => {
+  return <Container>RecruitmentsDetailPage</Container>;
+};
+
+export default RecruitmentsDetailPage;
