8c878e919b99cbe028d03b110db6e9399fb86470
chaKK <cdin23@naver.com>
2024-01-09 17:09:42 +0900

    feat: modal component, mypage darkmode

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -34,14 +34,16 @@ const Mypage = () => {
         <div className="flex flex-row gap-3">
           <Avatar src={userInfo?.profileImg} />
           <div className="flex flex-col justify-center">
-            <div className="text-xl font-bold">{userInfo?.nickname}</div>
-            <div className="text-sm text-slate-300 font-light">{userInfo?.email}</div>
+            <div className="text-xl font-bold dark:text-stone-100">{userInfo?.nickname}</div>
+            <div className="text-sm text-slate-300 font-light dark:text-stone-100">
+              {userInfo?.email}
+            </div>
           </div>
         </div>
 
         <div className="flex xl:flex-col md:flex-row sm:flex-row min-[320px]:flex-row gap-5">
           <div
-            className={`text-bold text-lg font-semibold cursor-pointer ${
+            className={`text-bold text-lg font-semibold cursor-pointer dark:text-stone-100 ${
               activeTab === 'MYHOME'
                 ? 'text-blue-500 xl:border-r-4 xl:border-b-0 min-[320px]:border-b-4 border-blue-500'
                 : ''
@@ -51,7 +53,7 @@ const Mypage = () => {
             MY 홈
           </div>
           <div
-            className={`text-bold text-lg font-semibold cursor-pointer  ${
+            className={`text-bold text-lg font-semibold cursor-pointer dark:text-stone-100  ${
               activeTab === 'SETTINGS'
                 ? 'text-blue-500 xl:border-r-4 xl:border-b-0 min-[320px]:border-b-4 border-blue-500'
                 : ''
@@ -66,43 +68,45 @@ const Mypage = () => {
         {activeTab === 'MYHOME' && (
           <div className="flex flex-col gap-20">
             <div className="flex flex-col gap-3">
-              <div className="text-2xl font-semibold">자기소개</div>
-              <div className="text-sm font-light">{userInfo?.introduce}</div>
+              <div className="text-2xl font-semibold dark:text-stone-100">자기소개</div>
+              <div className="text-sm font-light dark:text-stone-100">{userInfo?.introduce}</div>
             </div>
 
             <div className="flex flex-col gap-7">
-              <div className="text-2xl font-semibold">학교</div>
+              <div className="text-2xl font-semibold dark:text-stone-100">학교</div>
               <div className="flex flex-col gap-3">
-                <div className="text-md font-medium text-stone-700">학교명</div>
-                <div className="text-sm font-light">{userInfo?.school}</div>
+                <div className="text-md font-medium text-stone-700 dark:text-stone-100">학교명</div>
+                <div className="text-sm font-light dark:text-stone-100">{userInfo?.school}</div>
               </div>
 
-              <div className="text-lg font-medium">전공</div>
+              <div className="text-lg font-medium dark:text-stone-100">전공</div>
               <div className="flex flex-col gap-3">
-                <div className="text-sm font-medium text-stone-700">본전공</div>
-                <div className="text-sm font-light">
+                <div className="text-sm font-medium text-stone-700 dark:text-stone-100">본전공</div>
+                <div className="text-sm font-light dark:text-stone-100">
                   {userInfo?.majorCollege} {userInfo?.major}
                 </div>
               </div>
 
               <div className="flex flex-col gap-3">
-                <div className="text-sm font-medium text-stone-700">부전공 및 복수전공</div>
-                <div className="text-sm font-light">
+                <div className="text-sm font-medium text-stone-700 dark:text-stone-100">
+                  부전공 및 복수전공
+                </div>
+                <div className="text-sm font-light dark:text-stone-100">
                   {userInfo?.minorCollege} {userInfo?.major}
                 </div>
               </div>
             </div>
 
             <div className="flex flex-col gap-7">
-              <div className="text-2xl font-semibold">경력</div>
+              <div className="text-2xl font-semibold dark:text-stone-100">경력</div>
               <div className="flex flex-col gap-3">
-                <div className="text-md font-medium text-stone-700">직업</div>
-                <div className="text-sm font-light">{userInfo?.job}</div>
+                <div className="text-md font-medium text-stone-700 dark:text-stone-100">직업</div>
+                <div className="text-sm font-light dark:text-stone-100">{userInfo?.job}</div>
               </div>
 
               <div className="flex flex-col gap-3">
-                <div className="text-md font-medium text-stone-700">경력</div>
-                <div className="text-sm font-light">{userInfo?.experience}</div>
+                <div className="text-md font-medium text-stone-700 dark:text-stone-100">경력</div>
+                <div className="text-sm font-light dark:text-stone-100">{userInfo?.experience}</div>
               </div>
             </div>
           </div>
@@ -114,15 +118,15 @@ const Mypage = () => {
               className="flex flex-row items-center cursor-pointer"
               onClick={userInfoEditModal.onOpen}
             >
-              <div className="text-xl font-semibold">개인 정보 수정</div>
-              <div className="text-2xl ml-auto  font-light">{'>'}</div>
+              <div className="text-xl font-semibold dark:text-stone-100">개인 정보 수정</div>
+              <div className="text-2xl ml-auto  font-light dark:text-stone-100">{'>'}</div>
             </div>
             <div
               className="flex flex-row items-center cursor-pointer"
               onClick={passwordEditModal.onOpen}
             >
-              <div className="text-xl font-semibold">비밀번호 변경</div>
-              <div className="text-2xl ml-auto  font-light">{'>'}</div>
+              <div className="text-xl font-semibold dark:text-stone-100">비밀번호 변경</div>
+              <div className="text-2xl ml-auto  font-light dark:text-stone-100">{'>'}</div>
             </div>
           </div>
         )}
