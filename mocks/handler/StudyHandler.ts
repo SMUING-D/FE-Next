878799959e22209b711684d39a099cc85bed60e1
김용민 <dydals3440@gmail.com>
2024-01-20 03:40:46 +0900

    chore(msw setting): mSW setting

--- mocks/handler/StudyHandler.ts
+++ mocks/handler/StudyHandler.ts
@@ -1,28 +1,242 @@
-import { faker } from '@faker-js/faker';
+import { API_PATH } from '@/app/constants/path';
 import { HttpResponse, http } from 'msw';
 
-const User = [
-  {
-    email: 'dara@naver.com',
-    nickname: 'Dara',
-    image:
-      'https://images.unsplash.com/photo-1515041219749-89347f83291a?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    token: '12312312312'
-  },
-  {
-    email: faker.internet.email,
-    nickname: 'dydals3440',
-    image: faker.image.avatarGitHub,
-    token: faker.internet.password
-  }
-];
-
 export const StudyHandlers = [
-  http.post('/api/login', () => {
-    return HttpResponse.json(User[0], {
-      headers: {
-        'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/'
+  // 스터디 등록.
+  http.post(`${API_PATH.STUDIES}/users/:userId`, () => {
+    return HttpResponse.json({
+      isSuccess: true,
+      code: 200,
+      message: '성공합니다.'
+    });
+  }),
+  // 스터디 조회
+  http.get(`${API_PATH.STUDIES}/:studyId`, () => {
+    return HttpResponse.json({
+      isSuccess: true,
+      code: 200,
+      message: '성공하였습니다',
+      studyDto: {
+        title: '스터디 모집',
+        content: '스터디 모집입니다.',
+        startDate: '2020-01-01',
+        memberCount: 3,
+        dueDate: '2020-03-03',
+        college: '융합공과대학',
+        studyImageDtoList: [
+          {
+            studyImageId: 1,
+            studyImagePath: '/img/asdf.jpg'
+          },
+          {
+            studyImageId: 2,
+            studyImagePath: '/img/qwerty.jpg'
+          }
+        ],
+        commentList: [
+          // 댓글
+          {
+            id: 1,
+            stars: 5,
+            content: '저 참여하고 싶어요!',
+            studyId: 1,
+            createdAt: '2023-07-10 03:41:57',
+            updatedAt: '2023-07-10 03:41:57',
+            userDto: {
+              user_name: 'ㅁㅁ',
+              profile: '/profile/asdf.jpg'
+            },
+            commentReplyList: [
+              // 대댓글
+              {
+                id: 2,
+                likes: 5,
+                content: '참여하려면 어떻게 해야할까요?',
+                studyId: 1,
+                createdAt: '2023-07-10 03:41:57',
+                updatedAt: '2023-07-10 03:41:57',
+                userDto: {
+                  userName: 'ㅁㅁ',
+                  profile: '/profile/asdf.jpg' // 예시
+                }
+              },
+              {
+                id: 3,
+                likes: 5,
+                content: '최고에요요',
+                studyId: 1,
+                createdAt: '2023-07-10 03:41:57',
+                updatedAt: '2023-07-10 03:41:57',
+                userDto: {
+                  userName: 'ㅁㅁ',
+                  profile: '/profile/asdf.jpg' // 예시
+                }
+              }
+            ]
+          }
+        ],
+        updateAt: '2020-02-02'
       }
+    });
+  }),
+  // 스터디 전체 조회
+  http.get(`${API_PATH.STUDIES}`, () => {
+    return HttpResponse.json({
+      isSuccess: true,
+      code: 200,
+      message: '성공하였습니다',
+      studyListDto: [
+        {
+          studyDto: {
+            studyId: 1,
+            title: '스터디 모집',
+            content: '스터디 모집입니다.',
+            startDate: '2020-01-01',
+            memberCount: 3,
+            dueDate: '2020-03-03',
+            college: '융합공과대학',
+            studyImageDtoList: [
+              {
+                studyImageId: 1,
+                studyImagePath: '/img/asdf.jpg'
+              },
+              {
+                studyImageId: 2,
+                studyImagePath: '/img/qwerty.jpg'
+              }
+            ],
+            updateAt: '2020-02-02'
+          }
+        },
+        {
+          studyDto: {
+            studyId: {
+              studyId: 2,
+              title: '스터디 모집',
+              content: '스터디 모집입니다.',
+              startDate: '2020-01-01',
+              memberCount: 3,
+              dueDate: '2020-03-03',
+              college: '융합공과대학',
+              studyImageDtoList: [
+                {
+                  studyImageId: 1,
+                  studyImagePath: '/img/asdf.jpg'
+                },
+                {
+                  studyImageId: 2,
+                  studyImagePath: '/img/qwerty.jpg'
+                }
+              ],
+              updateAt: '2020-02-02'
+            }
+          }
+        }
+      ]
+    });
+  }),
+  // 스터디 삭제
+  http.delete(`${API_PATH.STUDIES}/:studyId`, () => {
+    return HttpResponse.json({
+      isSuccess: 'true',
+      code: 200,
+      message: '성공합니다'
+    });
+  }),
+  // 스터디 수정
+  http.patch(`${API_PATH.STUDIES}/:studyId`, () => {
+    return HttpResponse.json({
+      isSuccess: 'true',
+      code: 200,
+      message: '성공합니다'
+    });
+  }),
+  // 스터디 매칭
+  http.post(`${API_PATH.STUDIES}/users`, () => {
+    return HttpResponse.json({
+      isSuccess: 'true',
+      code: 200,
+      message: '성공합니다'
+    });
+  }),
+  // 스터디 댓글 달기
+  http.post(`${API_PATH.STUDIES}/:studyId/comments`, () => {
+    return HttpResponse.json({
+      isSuccess: 'true',
+      code: 200,
+      message: '성공합니다'
+    });
+  }),
+  // 스터디 댓글 삭제
+  http.delete(`${API_PATH.STUDIES}/:studyId/comments/:commentId`, () => {
+    return HttpResponse.json({
+      isSuccess: 'true',
+      code: 200,
+      message: '성공합니다'
+    });
+  }),
+  // 스터디 댓글 수정
+  http.patch(`${API_PATH.STUDIES}/:studyId/comments/:commentId`, () => {
+    return HttpResponse.json({
+      isSuccess: 'true',
+      code: 200,
+      message: '성공합니다'
+    });
+  }),
+  // 스터디 댓글 좋아요
+  http.patch(`${API_PATH.STUDIES}/comments/:commentId/likes`, () => {
+    return HttpResponse.json({
+      isSuccess: 'true',
+      code: 200,
+      message: '성공합니다'
+    });
+  }),
+  // 스터디 댓글 신고
+  http.patch(`${API_PATH.STUDIES}/comments/:commentId/reports`, () => {
+    return HttpResponse.json({
+      isSuccess: 'true',
+      code: 200,
+      message: '성공합니다'
+    });
+  }),
+  // 스터디 대댓글 달기
+  http.post(`${API_PATH.STUDIES}/comments/:commentId/comments`, () => {
+    return HttpResponse.json({
+      isSuccess: 'true',
+      code: 200,
+      message: '성공합니다'
+    });
+  }),
+  // 스터디 대댓글 삭제
+  http.delete(`${API_PATH.STUDIES}/comments/:commentId/comments`, () => {
+    return HttpResponse.json({
+      isSuccess: 'true',
+      code: 200,
+      message: '성공합니다'
+    });
+  }),
+  // 스터디 대댓글 수정
+  http.patch(`${API_PATH.STUDIES}/comments/:commentId/comments`, () => {
+    return HttpResponse.json({
+      isSuccess: 'true',
+      code: 200,
+      message: '성공합니다'
+    });
+  }),
+  // 스터디 대댓글 좋아요
+  http.patch(`${API_PATH.STUDIES}/comments/comments/:commentId/likes`, () => {
+    return HttpResponse.json({
+      isSuccess: 'true',
+      code: 200,
+      message: '성공합니다'
+    });
+  }),
+  // 스터디 대댓글 신고
+  http.patch(`${API_PATH.STUDIES}/comments/comments/:commentId/reports`, () => {
+    return HttpResponse.json({
+      isSuccess: 'true',
+      code: 200,
+      message: '성공합니다'
     });
   })
 ];
--- mocks/handler/postHandler.ts
+++ mocks/handler/postHandler.ts
@@ -1,28 +1,99 @@
-import { faker } from '@faker-js/faker';
+import { API_PATH } from '@/app/constants/path';
+import { JOB_POSTS, JOB_POST_DETAIL } from '@/app/constants/postData';
 import { HttpResponse, http } from 'msw';
 
-const User = [
-  {
-    email: 'dara@naver.com',
-    nickname: 'Dara',
-    image:
-      'https://images.unsplash.com/photo-1515041219749-89347f83291a?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    token: '12312312312'
-  },
-  {
-    email: faker.internet.email,
-    nickname: 'dydals3440',
-    image: faker.image.avatarGitHub,
-    token: faker.internet.password
-  }
-];
+// const User = [
+//   {
+//     email: 'dara@naver.com',
+//     nickname: 'Dara',
+//     image:
+//       'https://images.unsplash.com/photo-1515041219749-89347f83291a?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+//     token: '12312312312'
+//   },
+//   {
+//     email: faker.internet.email,
+//     nickname: 'dydals3440',
+//     image: faker.image.avatarGitHub,
+//     token: faker.internet.password
+//   }
+// ];
+
+const SuccessData = {
+  isSuccess: true,
+  code: 200,
+  message: '성공입니다'
+};
 
 export const PostHandlers = [
-  http.post('/api/login', () => {
-    return HttpResponse.json(User[0], {
-      headers: {
-        'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/'
-      }
-    });
+  // 전체 조회
+  http.get(`${API_PATH.POSTS}`, ({ request }) => {
+    const url = new URL(request.url);
+    const category = url.searchParams.get('category');
+    // const cursor = parseInt(url.searchParams.get('cursor') as string) || 0;
+    // const search = url.searchParams.get('search');
+
+    if (category === '전체') {
+      return HttpResponse.json();
+    }
+    if (category === '인문사회과학대학') {
+    }
+    if (category === '경영경제대학') {
+    }
+    if (category === '융합공과대학') {
+    }
+    if (category === '문화예술대학') {
+    }
+  }),
+  // 게시글 전체조회
+  http.post(`${API_PATH.JOBS}`, () => {
+    return HttpResponse.json(JOB_POSTS);
+  }),
+  // 게시글 상세조회
+  http.get(`${API_PATH.JOBS}/:jobId`, () => {
+    return HttpResponse.json(JOB_POST_DETAIL);
+  }),
+  // 게시글 작성
+  http.post(`${API_PATH.JOBS}`, () => {
+    return HttpResponse.json(SuccessData);
+  }),
+  // 게시글 삭제
+  http.delete(`${API_PATH.JOBS}/:jobId`, () => {
+    return HttpResponse.json(SuccessData);
+  }),
+  // 게시글 수정
+  http.patch(`${API_PATH.JOBS}/:jobId`, () => {
+    return HttpResponse.json(SuccessData);
+  }),
+  // 게시글 댓글 작성
+  http.post(`${API_PATH.JOBS}/:postId/comments`, () => {
+    return HttpResponse.json(SuccessData);
+  }),
+  // 댓글 신고
+  http.post(`${API_PATH.JOBS}/comments/:commentId/reports`, () => {
+    return HttpResponse.json(SuccessData);
+  }),
+  // 댓글 좋아요
+  http.post(`${API_PATH.JOBS}/comments/:commentId/likes`, () => {
+    return HttpResponse.json(SuccessData);
+  }),
+  // 댓글 삭제
+  http.delete(`${API_PATH.JOBS}/comments/:commentId`, () => {
+    return HttpResponse.json(SuccessData);
+  }),
+  // 게시글 대댓글 작성
+  http.post(`${API_PATH.JOBS}/comments/:reComments/comments`, () => {
+    return HttpResponse.json(SuccessData);
+  }),
+  // 게시글 대댓글 좋아요
+  http.post(`${API_PATH.JOBS}/comments/comments/:commentId/likes`, () => {
+    return HttpResponse.json(SuccessData);
+  }),
+  // 게시글 대댓글 신고
+  http.post(`${API_PATH.JOBS}/comments/comments/:commentId/reports`, () => {
+    return HttpResponse.json(SuccessData);
+  }),
+  // 게시글 대댓글 삭제
+  http.delete(`${API_PATH.JOBS}/comments/comments/:commentId`, () => {
+    return HttpResponse.json(SuccessData);
   })
 ];
