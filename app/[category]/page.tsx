3122e9cec9aab8fd7e3037eb5a1e94d3c7c51df8
김용민 <dydals3440@gmail.com>
2024-01-23 15:09:25 +0900

    feat([category]/page.tsx): 서버사이드에서 pathname 받기

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -1,20 +1,20 @@
 import { HydrationBoundary, QueryClient, dehydrate } from '@tanstack/react-query';
-import { headers } from 'next/headers';
 
 import Container from '../components/Container';
 import ListingContainerTest from '../components/listings/ListingContainerTest';
 import { getFilteredPosts } from '../lib/getFilteredPosts';
 
 type HomeProps = {
-  college: string;
+  params: {
+    category: string;
+  };
 };
 
-const CategoryDetailPage: React.FC<HomeProps> = async () => {
-  const headersList = headers();
-  const headerPathname = headersList.get('x-pathname') || '';
-  console.log('headerPathname: ', headerPathname);
+const CategoryDetailPage: React.FC<HomeProps> = async ({ params }) => {
+  const { category } = params;
+
   const queryClient = new QueryClient();
-  const college = 'coe';
+  const college = category;
 
   // 서버에서 불러온 데이터를 클라이언트의 리액트 쿼리가 물려받음.(하이드레이트)
   await queryClient.prefetchInfiniteQuery({
